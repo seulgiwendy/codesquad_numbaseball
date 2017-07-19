f589d501af5deecd5b875fcd39e95a2e35d898b9
wheej <wheej@DESKTOP-K5OAEF1>
2017-07-19 21:19:05 +0900

    -final commit

--- src/codesquad_numbaseball/App.java
+++ src/codesquad_numbaseball/App.java
@@ -1,12 +1,44 @@
 package codesquad_numbaseball;
+import java.util.Scanner;
 
 public class App {
 	
 	public static void main (String args[]) {
+		Scanner input = new Scanner(System.in);
+		boolean progSwitch = true;
+		int repeatTimes = 3;
+		int ballCount = 0;
+		int strikeCount = 0;
 		
-		int [] result = NumShuffler.returnRandint();
+		
+		/*int [] result = NumShuffler.returnRandint();
 		for (int i = 0; i < result.length; i++) {
 			System.out.println(result[i]);
+		}*/
+		
+		int [] computerBall = NumShuffler.returnRandint();
+		System.out.println("랜덤한 숫자 3개가 생성되었습니다. \n 숫자야구를 시작합니다!");
+		int [] userInputBall = new int [repeatTimes];
+		
+		while(progSwitch) {
+			
+			for (int i = 0; i < repeatTimes; i++) {
+				System.out.println(i + "번째 숫자 입력: ");
+				try {
+				userInputBall [i] = input.nextInt();}
+				catch(Exception e) {
+					e.printStackTrace();;
+				}
+				
+			}
+		
+		ballCount = Umpire.Ball(userInputBall, computerBall);
+		strikeCount = Umpire.Strike(userInputBall, computerBall);
+		System.out.println("볼카운트는 " + ballCount  + "B " + strikeCount + "S 입니다.");
+		if (strikeCount == 3) {
+			System.out.println("수뚜라익~~삼진!! 정답을 맞추셨습니다!");
+			progSwitch = false;
+		}
 		}
 	}
 
--- src/codesquad_numbaseball/Umpire.java
+++ src/codesquad_numbaseball/Umpire.java
@@ -1,12 +1,39 @@
 package codesquad_numbaseball;
 
+import java.util.ArrayList;
+
 public class Umpire {
 	
-	public static String StrikeBall(int[] player, int[] computer) {
-		int strikeCount, ballCount;
+	public static int Strike(int[] player, int[] computer) {
+		int strikeCount = 0; 
+		
+		for (int i = 0; i < player.length; i++) {
+			if (player[i] == computer [i]) {
+				strikeCount += 1;
+			}
+			
+		}
 		
 		
-		return null;
+		return strikeCount;
 	}
+	
+	public static int Ball(int [] player, int[] computer) {
+		int ballCount = 0;
+		ArrayList computerArray = new ArrayList<>();
+		for (int i = 0; i < computer.length; i++) {
+			computerArray.add(computer[i]);
+		}
+		
+		for (int i = 0; i < player.length; i++) {
+			
+			if (computerArray.contains(player[i]) && player[i] != computer[i]) {
+				ballCount += 1;
+			}
+		}
+	
+		return ballCount;
+	}
+	
 
 }
